5727dbe29fa2bf609ec5f0ecc90cf349f947a5bd
Alberto <alberto.moreno2357@gmail.com>
2024-03-03 14:05:36 +0100

    feat: searcher done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,9 +8,75 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
-      body: Container(
-        child: Text('Hello'),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          _searchField(),
+        ]
+      )
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xff1D1617).withOpacity(0.11),
+            spreadRadius: 0.0,
+            blurRadius: 40,
+          ),
+        ],
       ),
+      child: TextField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.only(left: 15),
+        prefixIcon: Padding(
+          padding: EdgeInsets.all(12),
+          child: SvgPicture.asset(
+            'assets/icons/Search.svg',
+            height: 5,
+            width: 5,
+          ),
+        ),
+        suffixIcon: Container(
+          width: 100,
+          child: IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                VerticalDivider(
+                  color: Color(0xffDDDADA),
+                  indent: 10,
+                  endIndent: 10,
+                  thickness: 0.1,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(12),
+                  child: SvgPicture.asset(
+                    'assets/icons/Filter.svg',
+                  ),
+                ),
+              ]
+            ),
+          ),
+        ),
+        hintText: 'Search Pancake',
+        hintStyle: TextStyle(
+          color: Color(0xffDDDADA),
+          fontSize: 14,
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    ),
     );
   }
 
