75f67d4d953f771dea77c124f1c2c4eff79302a6
Alberto <alberto.moreno2357@gmail.com>
2024-03-03 20:55:29 +0100

    feat: recommendations done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fitness/models/category_model.dart';
+import 'package:fitness/models/diet_model.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   List<CategoryModel> categories = [];
+  List<DietModel> diets = [];
 
   void _getCategories() {
     categories = CategoryModel.getCategories();
   }
 
+  void _getDiets() {
+    diets = DietModel.getDiets();
+  }
+
   @override
   Widget build(BuildContext context) {
     _getCategories();
+    _getDiets();
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
@@ -23,8 +30,96 @@ class HomePage extends StatelessWidget {
           _searchField(),
           SizedBox(height: 40),
           _categoriesSection(),
+          SizedBox(height: 40),
+          _recommedationDietSection()
         ]
       )
+    );
+  }
+
+  Column _recommedationDietSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text('Recommendation\nfor diet',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+        SizedBox(height: 15),
+        Container(
+          height: 240,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            itemCount: diets.length,
+            separatorBuilder: (context, index) => SizedBox(width: 25),
+            itemBuilder: (context, index) {
+              return Container(
+                width: 210,
+                decoration: BoxDecoration(
+                  color: diets[index].boxColor.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: SvgPicture.asset(
+                        diets[index].iconPath,
+                      ),
+                    ),
+                    Text(
+                      diets[index].name,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    Text(
+                      diets[index].level + ' | ' + diets[index].duration + ' | ' + diets[index].calorie,
+                      style: const TextStyle(
+                        color: Color(0xff7B6F72),
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400
+                      ),
+                    ),
+                    Container(
+                      height: 45,
+                      width: 130,
+                      child: Center(
+                        child: Text(
+                          'View',
+                          style: TextStyle(
+                            color: diets[index].viewIsSelected ? Colors.white : Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            diets[index].viewIsSelected ? const Color(0xff9DCEFF) : Colors.transparent,
+                            diets[index].viewIsSelected ? const Color(0xff92A3FD) : Colors.transparent
+                          ]
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                      ),
+                    )
+                  ]
+                ),
+              );
+            }
+          ),
+        )
+      ]
     );
   }
 
